a681c9ced72e1bfe96d9521331c0850e5b1ad70c
Davi S. Bento <davisilva4222@gmail.com>
2022-05-12 15:32:55 -0300

    viper and env file

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 
 	"github.com/davisbento/simple-bank-golang/api"
 	db "github.com/davisbento/simple-bank-golang/db/sqlc"
+	"github.com/davisbento/simple-bank-golang/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config file: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -26,7 +27,7 @@ func main() {
 
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
