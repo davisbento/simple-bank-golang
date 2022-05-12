548219953116d68c019d31aabda51a0b736e8d42
Davi S. Bento <davisilva4222@gmail.com>
2022-05-12 15:05:50 -0300

    install gin and create account

---
+++ api/account.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/davisbento/simple-bank-golang/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR GBP"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, account)
+}
---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "github.com/davisbento/simple-bank-golang/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for the API.
+type Server struct {
+	store  db.Store
+	router *gin.Engine
+}
+
+// New server creates a new server and setup routing.
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// Start run the HTTP server on specified address
+func (server *Server) Start(addr string) error {
+	return server.router.Run(addr)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
+}
